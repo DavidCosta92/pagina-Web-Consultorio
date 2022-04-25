3922e4a2e5b2892177b386a02532697af0e413ac
David Costa <davidcst2991@gmail.com>
2022-04-25 15:26:43 -0300

    slider especialidades

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const fila = document.querySelector('.contenedorSliderEsp');
+const especialidad = document.querySelectorAll('.especialidad');
+const flechaIzquierda =document.getElementById('flechaIzquierda');
+const flechaDerecha =document.getElementById('flechaDerecha');
+
+// --- event listener flecha derecha --- //
+/* scrollLeft => dice el valor numerico del scroll actual, 
+pero pasandole un valor, scrollLeft modificara el scroll a dicho valor */
+// offsetWidth => dice el valor del ancho del contenedor
+flechaDerecha.addEventListener('click', () => {
+    fila.scrollLeft += fila.offsetWidth; 
+   
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+  
+    if (indicadorActivo.nextSibling){
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+flechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth; 
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+  
+    if (indicadorActivo.previousSibling){
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+
+});
+
+// -- paginacion -- //
+// Math.ceil => redondea hacia arriba!
+
+const numeroDePaginas = Math.ceil(especialidad.length / 2);
+for ( let i = 0; i< numeroDePaginas; i++){
+    const indicador = document.createElement("button");
+    if(i===0){
+        indicador.classList.add('activo');
+    }
+    document.querySelector('.indicadores').appendChild(indicador);
+    indicador.addEventListener('click', (e) =>{
+        fila.scrollLeft = i * fila.offsetWidth;
+        document.querySelector('.indicadores .activo').classList.remove('activo');
+        e.target.classList.add('activo');
+    })
+}
